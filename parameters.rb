5350153abc4e30bd9ea426beba22609b910bca0a
Hara <harachung90@gmail.com>
2023-03-05 18:49:26 +1100

    Initial commit

---
+++ parameters.rb
@@ -0,0 +1,13 @@
+def add(first, second)
+  first + second
+end
+
+def subtract(first, second)
+  return first - second
+end
+
+puts add(100, 50)
+puts subtract(75, 25)
+
+total = add(3, 4)
+puts total
---
+++ variables.rb
@@ -0,0 +1,7 @@
+number = 4
+greeting = "Hello"
+
+puts number
+puts greeting
+puts number + 2
+puts 12 - number
