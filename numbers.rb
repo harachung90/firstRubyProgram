465ff47874e7f6dee9f745ba88cff8b744a8536f
Hara <harachung90@gmail.com>
2023-03-07 19:36:05 +1100

    Fourth commit. Numbers; Numeric types; math operations; operator precedence; converting strings to numbers

---
+++ numbers.rb
@@ -0,0 +1,4 @@
+whole_number = 12
+fractional_number = 12.0
+p whole_number.even?
+p fractional_number.even?
--- widgets.rb
+++ widgets.rb
@@ -1,9 +1,10 @@
 def ask(question)
   print question + " "
-  answer = gets.chomp
-  p "You entered #{answer} widgets."
+  gets.chomp
 end
 
 
 puts "Welcome to the widget store!"
-ask("How many widgets are you ordering?")
+answer = ask("How many widgets are you ordering?")
+number = answer.to_i
+puts "For #{number} widgets, your total is $#{number * 10}"
